76add8539dd91a58ede3ca1ce3f7a892988a85cc
Heitorss07 <160273343+Heitorss07@users.noreply.github.com>
2024-06-03 09:57:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Quais são os principais desafios enfrentados na preservação de idiomas ameaçados?",
+        alternativas: [
+            {
+                texto: "Os principais desafios na preservação de idiomas ameaçados incluem a falta de documentação, a migração das comunidades falantes, a influência de idiomas dominantes, e a falta de apoio governamental. A redução de falantes nativos, especialmente entre os jovens, também é um problema significativo.",
+                afirmacao: "Resposta Verdadeira"
+            },
+            {
+                texto: "Os desafios na preservação de idiomas ameaçados incluem a proibição legal desses idiomas, a ausência de tecnologia para documentação e ensino, e a indiferença total das comunidades falantes.",
+                afirmacao: "Resposta Falsa"
+            }
+        ]
+    },
+];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+mostraPergunta()
